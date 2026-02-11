10729dd96dad324d92d41db34cfbd4be79810376
ManviPrakash <prakashmanvi33@gmail.com>
2026-02-11 12:20:38 -0500

    adding the models

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
+    firstname: { type: String, required: true, trim: true },
+    lastname: { type: String, required: true, trim: true },
+    password: { type: String, required: true, minlength: 4 },
+    createon: { type: String, required: true }
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("User", userSchema);
